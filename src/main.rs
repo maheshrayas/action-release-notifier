0b1c854a262c4e5f8089a344c676857bd48be53b
rayasm1(Mahesh Rayas) <mahesh.rayas@anz.com>
2021-09-20 21:53:42 +1000

    create issue

--- src/main.rs
+++ src/main.rs
@@ -1,87 +1,136 @@
-use futures::future;
+use anyhow::Result;
+use chrono::{DateTime, Duration, Utc};
+use regex::Regex;
 use reqwest::header::HeaderMap;
 use serde::Deserialize;
-use std::{env};
+use std::collections::HashMap;
+use std::{env, process::exit};
 
 #[derive(Deserialize, Debug)]
 struct Release {
     name: String,
-    tag_name: String
+    tag_name: String,
+    published_at: String,
+    body: String,
 }
-#[derive(Deserialize, Debug)]
-struct User {
-    login: String,
+#[derive(Deserialize)]
+struct Issue {
+    html_url: String,
 }
 
 #[tokio::main]
-async fn main() -> Result<(), reqwest::Error> {
-    // let repo = env::var("INPUT_REPO").expect("Missing input parameter: repo");
-    // let org = env::var("INPUT_ORG").expect("Missing input parameter: org");
-    // let days = env::var("INPUT_ISSUE").unwrap_or_default();
-    let token = format!(
+async fn main() -> Result<()> {
+    process().await?;
+    Ok(())
+}
+
+async fn process() -> Result<(), reqwest::Error> {
+    let repo: &str = &env::var("INPUT_REPO").expect("Missing input parameter: repo");
+    let github_org: &str = &env::var("GITHUB_REPOSITORY").expect("Missing input parameter: repo"); // return org/reponame
+    let days: i64 = env::var("INPUT_DAYS")
+        .unwrap_or_else(|_| 1.to_string())
+        .parse::<i64>()
+        .unwrap();
+    let token = &format!(
         "token {}",
         env::var("INPUT_GITHUB_TOKEN").expect("Missing input parameter: github_token")
     );
+    println!("printing repos {}", repo);
+    let client = reqwest::Client::new();
+    let mut headers = HeaderMap::new();
+    headers.insert("Authorization", token.parse().unwrap());
+    headers.insert("Accept", "application/vnd.github.v3+json".parse().unwrap());
 
-    let github_org = env::var("GITHUB_REPOSITORY").expect("Missing input parameter: repo");
-    println!("GITHUB_ORG {}",github_org);
+    // strip org and repo from url
+    let m = match Regex::new(r"https://github.com/([\S]+)/([\S]+)") {
+        Ok(value) => {
+            match value.is_match(repo) {
+                true => {
+                    let m = value.captures(repo).unwrap();
+                    //println!("{:?}",&m);
+                    Some(m)
+                }
+                false => None,
+            }
+        }
+        Err(_) => {
+            println!("Failed to parse repo");
+            exit(1)
+        }
+    };
+    if let None = m {
+        panic!("Entered repo is not in valid http(s), ssh, git formtat")
+    };
+    let final_org = m.unwrap();
 
-    // let token = "";
-    // let repos = "action-pr-comment-delete";
-    // let org = "maheshrayas";
-    // //let days = env::var("INPUT_ISSUE").unwrap_or_default();
-    // let client = reqwest::Client::new();
-    // let mut headers = HeaderMap::new();
-    // headers.insert("Authorization", token.parse().unwrap());
-    // headers.insert(
-    //     "Accept",
-    //     "application/vnd.github.v3+jso".parse().unwrap(),
-    // );
-    // headers.insert("User-Agent", repos.parse().unwrap());
+    let (parsed_org, parsed_repo) = (&final_org[1], &final_org[2]);
 
-    // let url = format!(
-    //     "https://api.github.com/repos/{}/{}/releases/latest",
-    //     &org, &repos
-    // );
-    // let res = client.get(url).headers(headers.to_owned()).send().await?;
-    // let body: Option<Release> = match res.json::<Release>().await {
-    //     Ok(body) => {
-    //         Some(body)
-    //     },
-    //     Err(_) => {
-    //         None
-    //     },
-    // };
-    // let response = future::join_all(
-    //     body.iter()
+    println!("Org is {:?}, and repo is {:?}", parsed_org, parsed_repo);
+    headers.insert("User-Agent", repo.parse().unwrap());
+    let url = format!(
+        "https://api.github.com/repos/{}/{}/releases/latest",
+        &parsed_org, &parsed_repo
+    );
+    println!("Final url {:?}", &url);
+    let res = client.get(&url).headers(headers.to_owned()).send().await?;
+    if res.status() == 404 {
+        println!("No release found");
+    } else if res.status() == 200 {
+        let body = res.json::<Release>().await?;
+        if body.check_new_release(days) {
+            let current_repo: Vec<&str> = github_org.split("/").collect();
+            let curr_org = current_repo[0];
+            let curr_repo = current_repo[1];
+            println!("curr_org curr_repo body {} {:?}", curr_org, curr_repo);
+            let mut map = HashMap::new();
+            map.insert("title", "New issue");
 
-    //         .map(|pr_com| {
-    //             println!("Deleting comment id {}", pr_com.id);
-    //             client
-    //                 .delete(&pr_com.url)
-    //                 .headers(headers.to_owned())
-    //                 .send()
-    //         }),
-    // )
-    // .await;
-
-    //println!("Rsponse {:?}",body.unwrap());
-
-    // for b in response {
-    //     match b {
-    //         Ok(b) => println!("Got {} response", b.status()),
-    //         Err(e) => eprintln!("Got an error: {}", e),
-    //     }
-    // }
+            let url = format!(
+                "https://api.github.com/repos/{}/{}/issues",
+                &curr_org, &curr_repo
+            );
+            let res = client
+                .post(url)
+                .headers(headers.to_owned())
+                .json(&map)
+                .send()
+                .await?;
+            if res.status() == 201 {
+                let issue = res.json::<Issue>().await?;
+                println!("Created Issue at {}", issue.html_url);
+            }
+        }
+    } else {
+        println!("Error {}", res.status());
+    }
 
     Ok(())
+}
 
+impl Release {
+    fn check_new_release(&self, days: i64) -> bool {
+        let dt = Utc::now();
+        let x = dt - Duration::days(days);
+        let datetime = self.published_at.parse::<DateTime<Utc>>();
+        if let Ok(pub_date) = datetime {
+            if pub_date > x {
+                return true;
+            }
+        }
+        false
+    }
+}
 
-    // get the repo name
-    // frequency of release
-
-
-    // Query the repo and get the latest release
-    // check if the release is done in the x no of days
-    // if any release is found, create a github issue or slack notify
+#[tokio::test]
+async fn test_main() {
+    env::set_var(
+        "INPUT_REPO",
+        "https://github.com/maheshrayas/action-pr-comment-delete",
+    );
+    env::set_var("GITHUB_REPOSITORY", "maheshrayas/sample");
+    env::set_var("INPUT_GITHUB_TOKEN", "blah");
+    if let Err(_) = process().await {
+        panic!("Failed",);
+    }
+    println!("Success");
 }
